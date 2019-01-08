583f8a109a72872ae3cdc56d3a7c942586266111
pcq <1152684231@qq.com>
2019-01-08 17:42:46 +0800

    增加了一些素材图片

--- dice/dice.js
+++ dice/dice.js
@@ -20,20 +20,35 @@ function createGeometry() {
 
     geometry = new THREE.BoxGeometry(1, 1, 1, 4, 4, 4);
 
-    var material1 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/crate.jpg') });
-    var material2 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/bricks.jpg') });
-    var material3 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/clouds.jpg') });
-    var material4 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/stone-wall.jpg') });
-    var material5 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/water.jpg') });
-    var material6 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/wood-floor.jpg') });
+    // 下面这种是为每个面都加一个贴图，但是随着模型的增多
+    //
+    // var material1 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/crate.jpg') });
+    // var material2 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/bricks.jpg') });
+    // var material3 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/clouds.jpg') });
+    // var material4 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/stone-wall.jpg') });
+    // var material5 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/water.jpg') });
+    // var material6 = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('../img/wood-floor.jpg') });
+    // var materials = [material1, material2, material3, material4, material5, material6];
+    // for (let i = 0; i < 6; i++) {
+    //     meshFaceMaterial.push(materials[i]);
+    // }
+    // var mesh = new THREE.Mesh(geometry, meshFaceMaterial);
 
-    var materials = [material1, material2, material3, material4, material5, material6];
+    texture = new THREE.TextureLoader().load('../img/texture-atlas-1.jpg');
+    texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    texture.anisotropy = 16;
 
-    for (let i = 0; i < 6; i++) {
-        meshFaceMaterial.push(new THREE.MeshBasicMaterial(materials[i]));
-    }
-    
-    var mesh = new THREE.Mesh(geometry, meshFaceMaterial);
+    material = new THREE.MeshBasicMaterial({ map: texture });
+
+    var bricks = [new THREE.Vector2(0, .666), new THREE.Vector2(.5, .666), new THREE.Vector2(.5, 1), new THREE.Vector2(0, 1)];
+
+
+    geometry.faceVertexUvs[0] = [];
+
+    geometry.faceVertexUvs[0][0] = [bricks[0], bricks[1], bricks[3]];
+    geometry.faceVertexUvs[0][1] = [bricks[1], bricks[2], bricks[3]];
+
+    var mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 }
 
@@ -45,3 +60,9 @@ function run() {
 init();
 createGeometry();
 run();
+
+// var clouds = [new THREE.Vector2(.5, .666), new THREE.Vector2(1, .666), new THREE.Vector2(1, 1), new THREE.Vector2(.5, 1)];
+// var crate = [new THREE.Vector2(0, .333), new THREE.Vector2(.5, .333), new THREE.Vector2(.5, .666), new THREE.Vector2(0, .666)];
+// var stone = [new THREE.Vector2(.5, .333), new THREE.Vector2(1, .333), new THREE.Vector2(1, .666), new THREE.Vector2(.5, .666)];
+// var water = [new THREE.Vector2(0, 0), new THREE.Vector2(.5, 0), new THREE.Vector2(.5, .333), new THREE.Vector2(0, .333)];
+// var wood = [new THREE.Vector2(.5, 0), new THREE.Vector2(1, 0), new THREE.Vector2(1, .333), new THREE.Vector2(.5, .333)];
--- init.js
+++ init.js
@@ -3,6 +3,7 @@ var geometry, material, mesh;
 var controls;
 
 
+
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(1000, window.innerWidth / window.innerHeight, 1, 100);
@@ -13,50 +14,12 @@ function init() {
 
     controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-
-    for (let i = 0; i < 5; i++) {
-        create("立方体");
-    }
-
-    // for (let i = 0; i < scene.children.length; i++) {
-    //     if (i <= 2) {
-    //         scene.children[i].position.x = i * 2;
-    //         scene.children[i].position.y = i * 2;
-    //         scene.children[i].position.z = i * 2;
-    //     }
-    //     else if (i >= 3) {
-    //         scene.children[i].position.x = -i*3;
-    //         scene.children[i].position.y = -i*3;                                                            
-    //         scene.children[i].position.z = -i*3;
-    //     }
-
-    // }
-
-    scene.children[1].position.x = 2;
-    scene.children[1].position.y = 2;
-    scene.children[1].position.z = -2;
+    var earthGeometry = new THREE.SphereBufferGeometry();
+    var earthMaterial = new THREE.MeshPhongMaterial({
+        specular: 0x333333,
+        shininess: 30
+    });
 }
-
-function create(shape) {
-    material = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x00BFFF });
-    switch (shape) {
-        case "球形":
-            var texture = new THREE.TextureLoader().load('http://192.168.0.67:5500/img/map.jpg');// 图片的比例一定要是2:1
-            material = new THREE.MeshBasicMaterial({ map: texture });
-            geometry = new THREE.SphereGeometry(1, 32, 32, 0, 6.3, 0, 3.2);
-            break;
-        case "立方体":
-            geometry = new THREE.BoxGeometry(2, 2, 2, 2, 2, 2); // 立方体
-            break;
-        default:
-            break;
-    }
-    scene.add(new THREE.Mesh(geometry, material));
-}
-
-init();
-
-run();
 
 function run() {
     scene.rotation.y += 0.02;
@@ -64,3 +27,6 @@ function run() {
     renderer.render(scene, camera);
     requestAnimationFrame(run);
 }
+
+init();
+run();
